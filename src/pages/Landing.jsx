e0d73fe6013629d4a3559986b893e6d062a6ccdc
Jacob <jacob@militarytransitiontoolkit.com>
2025-11-13 00:03:47 -0800

    Add deadline banner to public landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import DeadlineBanner from '../components/DeadlineBanner'
 
 export default function Landing() {
   return (
@@ -55,6 +56,9 @@ export default function Landing() {
           </div>
         </div>
       </nav>
+
+      {/* Founding Member Deadline Banner with Countdown */}
+      <DeadlineBanner />
 
       {/* Government Shutdown Support Banner */}
       <div className="bg-gradient-to-r from-yellow-500 via-amber-500 to-yellow-500 border-b-4 border-yellow-600">
